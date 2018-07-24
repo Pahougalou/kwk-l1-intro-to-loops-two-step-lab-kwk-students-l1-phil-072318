c7b0cd9e16ce74e26ca8ad8c2cf956eab3b3d90a
Reice Griffin <reicelovesllamas@gmail.com>
2018-07-24 20:07:56 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -30,10 +30,25 @@
 
 # a_few_more_steps
 
-def how_many_steps?
+def how_many_steps
 steps = 0 
-
+loop do 
+  if step%==0 
+    puts left 
+    elsif 
+    puts right 
+  steps = +1
+  end
 end
+
+how_many_steps
+
+# * Just below this variable, write a loop that does four things:
+#   1. Increases the value of `steps` by 1
+#   2. Outputs the value of `steps`
+#   3. If the value of `steps` is divisible by 2 (remember how to use the `%`?),
+# outputs 'Left', otherwise, it should output 'Right'
+#   3. Sleeps for 0.5 seconds
 
 def break_dance
   # Write a solution that uses the same code as how_many_steps?, but breaks the
